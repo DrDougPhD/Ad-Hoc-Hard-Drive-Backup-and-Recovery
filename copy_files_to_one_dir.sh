f7c6df60b67d4ea3c3f65961525bdf1d31a067d6
Doug McGeehan <djmvfb@mst.edu>
2016-11-02 16:13:22 -0500

    if the source file doesn't exist, error. if there already exists a dest file with that name, then print message

--- copy_files_to_one_dir.sh
+++ copy_files_to_one_dir.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 ROOT="$2"
 while read URL; do
-	#FILENAME=$( /usr/bin/basename "$PATH" )
-	#echo -e "$FILENAME \t $PATH" 
 	if [ ! -z "${URL// }" ]
-	then
-		ln -s "${ROOT}${URL}" ./
-		#if [ -e $(basename "${URL}") ]
-		#then
-		#	echo "DUPLICATE FILE"
-		#else
-		#fi
+	then	# the current URL is not empty
+		ABSPATH="${ROOT}${URL}"
+		if [ -e "$ABSPATH" ]
+		then	# file exists where it should
+			echo "Checking if file exists..."
+			if [ -e ./"$( basename '${URL}' )" ]
+			then	# there exists a file here with that name
+				echo "DUPLICATE FILE"
+			else
+				ln -s "${ROOT}${URL}" ./
+			fi
+		else
+			echo "Source file doesn't exist:" ${ABSPATH}
+		fi
 	fi
 done < $1
 
