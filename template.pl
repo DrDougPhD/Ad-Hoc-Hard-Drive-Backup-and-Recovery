74da7ca034daf9e231a194d9ceeacdc478d4137d
Doug McGeehan <djmvfb@mst.edu>
2016-11-02 19:23:00 -0500

    iterating over lines in file

--- template.pl
+++ template.pl
@@ -1,8 +1,11 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+
 use feature 'say';
+use autodie; # die if problem reading or writing a file
 use Getopt::Long qw(GetOptions);
+
 
 my $files_in_dir = "/media/kp/bde47223-0184-4d33-a224-c3622c62920c/Backup/jazzpony/";
 my $file_list = "/home/kp/Documents/Backup_Review/Jazzpony/sample.txt";
@@ -16,3 +19,21 @@ GetOptions(
 say $files_in_dir;
 say $file_list;
 say $dir_to_store_files;
+say "-------------------";
+
+open(my $fh, '<:encoding(UTF-8)', $file_list)
+	or die "Could not open file '$file_list' $!";
+
+while (my $row = <$fh>) {
+	chomp $row;
+	say $row;
+}
+
+
+# openr() returns an IO::File object to read from
+#my $file_handle = $file->openr();
+
+# Read in line at a time
+#while( my $line = $file_handle->getline() ) {
+#        print $line;
+#}
