d9b4afbe6e265e40185025576d1d4144eb34cb9d
Doug McGeehan <djmvfb@mst.edu>
2016-11-05 14:59:45 -0500

    script to prepare for purging of duplicate files

--- duplicates.sh
+++ duplicates.sh
@@ -1,2 +1,18 @@
 #!/usr/bin/env bash
+#
+# Build a list of duplicate files stored within a directory.
+#
+# Copyright 2016 Doug McGeehan. All rights reserved.
+#
+#------------------------------------------------------------------------------
+#
+# TODO:
+#  Absolute paths for each duplicate file
+#  File groups sorted by size
+#  Replace a duplicate file with a symbolic link of the same name
+#  Obtain user/group, creation times, modification times of files
+#  Sort files within group by date, from oldest to newest
+#
+echo "Current working directory: $(pwd)"
+echo "------------------------------------------------------------------------"
 fdupes --recurse --size "$1"
