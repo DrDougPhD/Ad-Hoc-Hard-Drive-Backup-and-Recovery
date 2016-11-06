d5bb061927243ec3ff47d6c48f557e3153009f0b
Doug McGeehan <djmvfb@mst.edu>
2016-11-05 19:26:40 -0500

    reading in csv, grouping by size, removing files with unique file sizes

--- intelligent_duplicate_checksums.py
+++ intelligent_duplicate_checksums.py
@@ -8,6 +8,17 @@ Intelligent checksums and duplicate detection. Iterate over all files,
 
  Usage: <blah>.sh < files,sizes.txt > files,sizes,checksums.txt
 
+ Output:
+$path	$bytes	$owner	$group	$timestamps	$md5sums
+
+Read input csv file
+Group by file size
+Filter out singleton groups
+Transform by md5sums
+Perhaps group by md5sums now
+Sort by timestamp
+Output to file
+
 Copyright 2016 Doug McGeehan. All rights reserved.
 
 -------------------------------------------------------------------------------
@@ -20,7 +31,7 @@ TODO:
 """
 
 import sys
-import panda
+import pandas
 import numpy
 
 
@@ -31,16 +42,23 @@ if __name__ == "__main__":
 	file's size in bytes. Columns are separated by tabs. All other columns are
 	ignored.
 	"""
-	files = pandas.read_csv(
+	all_file_info = pandas.read_csv(
 		filepath_or_buffer=sys.argv[1],
 		sep='\t',
 		names=['url', 'bytes', 't1', 't2', 't3'],
 		usecols=['url', 'bytes'],
 		dtype={'url': str, 'bytes': numpy.uint64},
-		chunksize=100,
 		compression='infer',
 		quotechar="'"
 	)
+	print("{0} raw files {0}".format('-'*20))
+	print(all_file_info)
+	print("{0} grouped by size {0}".format('-'*20))
+	print(all_file_info.groupby('bytes').groups)
+	print("{0} files w/ duplicate sizes {0}".format('-'*20))
+	same_sized_groups = all_file_info.groupby('bytes')\
+		.filter(lambda group: len(group) > 1)
+	print(same_sized_groups)
 
 	"""
 	groupby('bytes').fn()
