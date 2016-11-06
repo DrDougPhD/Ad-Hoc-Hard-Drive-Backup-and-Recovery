05c24b8b926ba236f2c89051ac75db1d08d86f76
Doug McGeehan <djmvfb@mst.edu>
2016-11-06 15:53:01 -0600

    it was breaking if you called it to navigate from the root dir, and was having problems with the file path if there was a single apostrophe  in it

--- recursive_file_list.sh
+++ recursive_file_list.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # remove a dangling forward slash from directory
-DIRECTORY=$(echo "$1" | sed 's,/$,,g') 
+#DIRECTORY=$(echo "$1" | sed 's,/$,,g') 
+DIRECTORY="$1"
 
-find "$DIRECTORY" -type f -printf "'${DIRECTORY}/%P'\t%s\t%A@\t%C@\t%T@\n"
+find "$DIRECTORY" -type f -printf "${DIRECTORY}/%P\t%s\t%A@\t%C@\t%T@\n"
