74980218dd204335fa0c1a32ca985d7b523341bf
Doug McGeehan <djmvfb@mst.edu>
2016-11-06 21:54:04 -0600

    prompt user for cd name and loop continually

--- backdatdiscup.sh
+++ backdatdiscup.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
+
+echo -n "Enter CD name and press [ENTER]: "
+
 while read cd_name
 do
-  echo "${cd_name}"
+	# check if nothing was input
+	if [[ -z "${cd_name// }" ]]
+	then
+		exit
+	fi
+
+	echo "================================================================"
+	echo "Reading CD '${cd_name}'"
+	#isoinfo -p -d -i /dev/cdrom >"${cd_name}.isoinfo.txt"
+	#ddrescue -n -b 2048 /dev/cdrom "${cd_name}.iso" "${cd_name}.log"
+
+	echo "------------------= done done done done done =------------------"
+	echo -n "Enter CD name and press [ENTER]: "
 done < "${1:-/dev/stdin}"
