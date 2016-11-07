aec812dc7eecb25979ea615f12fc3994d02d7b02
Doug McGeehan <djmvfb@mst.edu>
2016-11-07 13:50:54 -0600

    todos added

--- backdatdiscup.sh
+++ backdatdiscup.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
+#
+# TODO
+#	Continuously loop until disc is inserted and ready
+#	Detect when blank disc is inserted and notify user
+#	When image has been duplicated, mount image and copy files to secondary
+#		location.
+#
+USER_PROMPT="Enter CD name and press [ENTER]: "
+INFO_DIR="info"
+IMG_DIR="img"
+LOG_DIR="log"
 
-echo -n "Enter CD name and press [ENTER]: "
+mkdir -p "${INFO_DIR}"
+mkdir -p "${IMG_DIR}"
+mkdir -p "${LOG_DIR}"
+
+echo -n "${USER_PROMPT} "
 
 while read cd_name
 do
@@ -12,9 +27,15 @@ do
 
 	echo "================================================================"
 	echo "Reading CD '${cd_name}'"
-	#isoinfo -p -d -i /dev/cdrom >"${cd_name}.isoinfo.txt"
-	#ddrescue -n -b 2048 /dev/cdrom "${cd_name}.iso" "${cd_name}.log"
+	blkid /dev/sr0			>"${INFO_DIR}/${cd_name}.blkid.txt"
+	isoinfo -p -d -i /dev/sr0	>"${INFO_DIR}/${cd_name}.isoinfo.txt"
+	ddrescue	--no-scrape \
+			--sector-size=2048 \
+			/dev/sr0					\
+			"${IMG_DIR}/${cd_name}.img" \
+			"${LOG_DIR}/${cd_name}.ddrescue.log"
+	eject /dev/sr0
 
 	echo "------------------= done done done done done =------------------"
-	echo -n "Enter CD name and press [ENTER]: "
+	echo -n "${USER_PROMPT} "
 done < "${1:-/dev/stdin}"
