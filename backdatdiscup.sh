3cd9b13c4ac9904bf59cdac302cad96f99d1d68a
Doug McGeehan <djmvfb@mst.edu>
2016-11-07 15:42:38 -0600

    waiting for disc, auto mirroring, mounting and copying to secondary drive

--- backdatdiscup.sh
+++ backdatdiscup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #
 # TODO
+#	Detect if inserted disc is a music CD
 #	Continuously loop until disc is inserted and ready
 #	Detect when blank disc is inserted and notify user
 #	When image has been duplicated, mount image and copy files to secondary
@@ -10,12 +11,16 @@ USER_PROMPT="Enter CD name and press [ENTER]: "
 INFO_DIR="info"
 IMG_DIR="img"
 LOG_DIR="log"
+SECONDARY_DST="/media/kp/AB58-B7AF/CD Backups"
+
+EMPTY_DRIVE=0
+BLANK_DISC=2048
 
 mkdir -p "${INFO_DIR}"
 mkdir -p "${IMG_DIR}"
 mkdir -p "${LOG_DIR}"
 
-echo -n "${USER_PROMPT} "
+echo -n "${USER_PROMPT}"
 
 while read cd_name
 do
@@ -25,9 +30,33 @@ do
 		exit
 	fi
 
-	echo "================================================================"
-	echo "Reading CD '${cd_name}'"
+	# wait until disc is ready
+	echo "Waiting for disc"
+	cd_drive_size=$EMPTY_DRIVE
+	while [[ $cd_drive_size -le $BLANK_DISC ]]
+	do
+		echo -ne "."
+		cd_drive_size=$(udisksctl info --block-device /dev/sr0 | grep 'Size:' | awk -F' ' '{ print $2 }' -)
+		if	[[ $cd_drive_size -eq $EMPTY_DRIVE ]]
+		then
+			sleep 1
+		elif	[[ $cd_drive_size -eq $BLANK_DISC ]]
+		then
+			echo
+			echo "Blank disc in drive. Ejecting."
+			eject /dev/sr0
+			sleep 1
+			cd_drive_size=$EMPTY_DRIVE
+		else
+			echo
+			echo "Disc found! Begin mirroring."
+		fi
+	done
+
+	# gather metadata and begin mirroring
 	blkid /dev/sr0			>"${INFO_DIR}/${cd_name}.blkid.txt"
+	udisksctl info --block-device /dev/sr0 \
+					>"${INFO_DIR}/${cd_name}.udisks.txt"
 	isoinfo -p -d -i /dev/sr0	>"${INFO_DIR}/${cd_name}.isoinfo.txt"
 	ddrescue	--no-scrape \
 			--sector-size=2048 \
@@ -35,6 +64,11 @@ do
 			"${IMG_DIR}/${cd_name}.img" \
 			"${LOG_DIR}/${cd_name}.ddrescue.log"
 	eject /dev/sr0
+	echo "------------------= copy files to secondary  =------------------"
+	mkdir -p ~/mirror_img
+	mount -o loop,ro "${IMG_DIR}/${cd_name}.img" ~/mirror_img
+	rsync -rltzuvP ~/mirror_img/ "${SECONDARY_DST}/${cd_name}"
+	umount ~/mirror_img
 
 	echo "------------------= done done done done done =------------------"
 	echo -n "${USER_PROMPT} "
