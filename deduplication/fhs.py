cdb737e21730a818bcb6a05d3ee02fddd3c018bc
Doug McGeehan <djmvfb@mst.edu>
2016-11-10 02:20:38 -0600

    I think I have the API locked down, though it might need some improvement

--- deduplication/fhs.py
+++ deduplication/fhs.py
@@ -25,6 +25,12 @@ class FileBundler:
 		self.file_bundles = []
 
 	def filter(self, function):
+		return self
+
+	def __lt__(self, other):
+		return len(self) < len(other)
+
+	def overhead(self):
 		pass
 
 	"""
@@ -53,6 +59,7 @@ def filehasher(file_path, block_size=_4MiB):
 
 
 def thematic_break(title=None, char='-', width=80):
+	#TODO: if len("- " + title + " -") > width, then split in half nicely
 	if not title:
 		return char*width
 
@@ -69,7 +76,9 @@ def thematic_break(title=None, char='-', width=80):
 	)
 	if len(line)==(width-1):
 		line += char
-	assert len(line)==width, "Line length is not of expected length (expected: {width}, actual: {line_width})\n{line}".format(width=width, line_width=len(line), line=line)
+	assert len(line)==width, ("Line length is not of expected length "
+		"(expected: {width}, actual: {line_width})\n{line}".format(
+		width=width, line_width=len(line), line=line))
 	return line
 
 
@@ -87,16 +96,74 @@ if __name__ == "__main__":
 	print(files)
 	print(thematic_break())
 
-	"""
+	print(thematic_break(title="STAGE 2", char="="))
+	print(thematic_break(title="remove singleton file bundles"))
 	only_multifile_bundles = lambda bundle: 1 < len(bundle)
 	#potential_duplicates = filter(only_multifile_bundles, files)
 	potential_duplicates = files.filter(only_multifile_bundles)
 	print(potential_duplicates)
+	print(thematic_break())
 
-	print(filehasher(file_path=directory/'file.1'))
+	print(thematic_break(title="STAGE 3", char="="))
+	print(thematic_break(title="compute file hashes", char="-"))
 	hashed_files = FileBundler(
 		within=potential_duplicates,
-		key=filehasher
+		key=hash
 	)
+	print(potential_duplicates)
+	print(thematic_break())
+
+
+	print(thematic_break(title="STAGE 4", char="="))
+	print(thematic_break(title="focus on files with duplicate copies"))
 	duplicate_files = hashed_files.filter(only_multifile_bundles)
-	""" 
+	print(duplicate_files)
+	print(thematic_break())
+	#print(filehasher(file_path=directory/'file.1'))
+
+	print(thematic_break(title="STAGE 5", char="="))
+	print(thematic_break(
+		title="sort bundles by size reduction through pruning",
+	))
+	size_reduction_from_pruning = lambda bundle: bundle
+	sorted_bundles_of_duplicate_file = sorted(
+		duplicate_files, key=redundancy_overhead, reversed=True
+	)
+	print(sorted_dupes)
+	print(thematic_break())
+
+	print(thematic_break(title="STAGE 6", char="="))
+	print(thematic_break(title="sort by file age within each bundle"))
+	sort_bundle_by_file_age = lambda bundle: sorted(bundle, key=blah)
+
+	further_sorted_by_file_age = map(
+		sort_bundle_by_file_age,
+		sorted_bundles_of_duplicate_files,
+	)
+	print(sorted_dupes)
+	print(thematic_break())
+
+	#--- list files to delete?
+	# #original :=> size, age, truncated_url, 3 duplicate copies
+	for bundle in further_sorted_by_file_age:
+		print(bundle.summary())
+
+		for file in bundle:
+			if file == bundle.oldest():
+				print("# original :=> {truncated_url} ({size}, {age}), {n} duplicate copies".format(
+					truncated_url=file.truncate_url(),
+					size=len(file),
+					age=file.age(),
+					n=len(bundle)-1
+				))
+			else:
+				print("{truncated_url}\t{size}\t{age}\t{space_recovered}",
+					space_recovered=cum_size_reduction_if_deleted,
+					size=len(file),
+					age=file.age(),
+					truncated_url=file.truncate_url()
+				)
+
+	# cumulative_size, age, truncated_url
+#hash(obj)
+
