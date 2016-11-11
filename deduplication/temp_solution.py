a197dd4b3712f62f40cde0b2176e8dc0e9a4ba88
Doug McGeehan <djmvfb@mst.edu>
2016-11-11 03:08:42 -0600

    truncate filenames if needed

--- deduplication/temp_solution.py
+++ deduplication/temp_solution.py
@@ -1,8 +1,7 @@
-import sys
 import humanize
 import os
-
-SIZE_THRESHOLD = 1000*1000*1000
+import argparse
+from pathlib import Path
 
 def truncate_url(path, to_length=70):
 	dirname, filename = os.path.split(path)
@@ -14,6 +13,7 @@ def truncate_url(path, to_length=70):
 		path_parts = path_parts[1:]
 		dirname = '.../' + '/'.join(path_parts) + '/'
 		if not path_parts:
+			dirname = dirname[:-1] # remove trailing slash "..//"
 			break
 
 	# if filename is still to long...
@@ -33,7 +33,45 @@ def truncate_url(path, to_length=70):
 	return dirname + filename
 
 
-with open(sys.argv[1]) as f:
+def get_arguments():
+	parser = argparse.ArgumentParser(
+		description=("Construct a shell script to delete duplicate "
+			"files, potentially replacing them with symbolic "
+			"links")
+	)
+	parser.add_argument('-v', '--verbose', action='store_true',
+		default=True, help='verbose output')
+	parser.add_argument('-d', '--within-directory',
+		dest='within_directory',
+		default='.', type=Path,
+		help='path to directory in which to search for dupes',
+	)
+	parser.add_argument('-f', '--fdupes-file',
+		dest='fdupes_file', type=Path,
+		required=True,
+		help='pre-existing fdupes file with filesizes',
+	)
+	parser.add_argument('-g', '--space-recovery-goal',
+		dest='bytes_to_reclaim',
+		help=('How much space is desired to be reclaimed. '
+			'Delete enough files to reach this goal.'),
+		default=float('inf')
+	)
+	parser.add_argument('-s', '--replace-with-symlinks',
+		dest='replace_with_symlinks', action='store_true',
+		default=False,
+		help="instead of outright deleting, replace files with symlinks"
+	)
+	parser.add_argument('-m', '--min-duplicate-size-to-delete',
+		dest='min_dupe_group_size',
+		default=0, type=int,
+		help='groups taking up less than this size will not be deleted',
+	)
+
+	return parser.parse_args()
+
+args = get_arguments()
+with open(str(args.fdupes_file)) as f:
 	redundant_files = []
 	for line in f:
 		line = line.strip()
@@ -52,42 +90,59 @@ with open(sys.argv[1]) as f:
 				files.append(line)
 
 	redundant_files.sort(key=lambda group: group['savings_if_deduped'], reverse=True)
-	large_enough_savings = lambda grp: grp['savings_if_deduped'] > SIZE_THRESHOLD
+	large_enough_savings = lambda grp: grp['savings_if_deduped'] > args.min_dupe_group_size
 
 	# summarize the files that would be deleted, along with how much space
 	# is reclaimed by deleting all files above that line
-	running_total_savings = 0
 	print('#'*80)
 	print("# {path:^60}  {size:^6}  {savings:^6} #".format(
 		path="Files to delete (truncated paths)",
 		size="Size",
 		savings="Save"
 	))
-	for duplicate_group in filter(large_enough_savings, redundant_files):
-		files_to_delete = duplicate_group['files'][1:]
-		for file in files_to_delete:
-			running_total_savings += duplicate_group['file_size']
-			print("# {path:>60}  {size:>6}  {running_total_savings:>6} #".format(
-				path=truncate_url(file, to_length=60),
-				size=humanize.naturalsize(
-					duplicate_group['file_size'],
-					gnu=True
-				),
-				running_total_savings=humanize.naturalsize(
-					running_total_savings,
-					gnu=True
-			)))
+
+	running_total_savings = 0
+	duplicates = list( filter(large_enough_savings, redundant_files) )
+	try:
+		for duplicate_group in duplicates:
+			files_to_delete = duplicate_group['files'][1:]
+			for file in files_to_delete:
+				running_total_savings += duplicate_group['file_size']
+				print("# {path:>60}  {size:>6}  {running_total_savings:>6} #".format(
+					path=truncate_url(file, to_length=60),
+					size=humanize.naturalsize(
+						duplicate_group['file_size'],
+						gnu=True
+					),
+					running_total_savings=humanize.naturalsize(
+						running_total_savings,
+						gnu=True
+				)))
+				if running_total_savings >= args.bytes_to_reclaim:
+					raise
+	except: # no need to continue iterating once goal is reached
+		pass
+
 	print('#'*80)
 
-	# create shell commands to perform deletion
-	for duplicate_group in filter(large_enough_savings, redundant_files):
-		original_file = duplicate_group['files'][0]
-		print("#"*40 + " Original: '{}'".format(original_file))
-		files_to_delete = duplicate_group['files'][1:]
-		for file in files_to_delete:
-			print("rm '{}'".format(file))
-			print("ln -s '{original_file}' '{deleted_file_path}'".format(
-				original_file=original_file,
-				deleted_file_path=file,
-			))
+	running_total_savings = 0
+	try:
+		# create shell commands to perform deletion
+		for duplicate_group in filter(large_enough_savings, redundant_files):
+			original_file = duplicate_group['files'][0]
+			print("#"*40 + " Original: '{}'".format(original_file))
+			files_to_delete = duplicate_group['files'][1:]
+			for file in files_to_delete:
+				running_total_savings += duplicate_group['file_size']
+				print("rm '{}'".format(file))
+				if args.replace_with_symlinks:
+					print("ln -s '{original_file}' '{deleted_file_path}'".format(
+						original_file=original_file,
+						deleted_file_path=file,
+					))
+				if running_total_savings >= args.bytes_to_reclaim:
+					raise
+
+	except:
+		pass
 
