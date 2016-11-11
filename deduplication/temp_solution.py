aef779a4408b4b0a34c8e80512afd27d636cd827
Doug McGeehan <djmvfb@mst.edu>
2016-11-11 02:34:54 -0600

    if filename is too long, shorten it the same way

--- deduplication/temp_solution.py
+++ deduplication/temp_solution.py
@@ -2,7 +2,7 @@ import sys
 import humanize
 import os
 
-SIZE_THRESHOLD = 1000*1000
+SIZE_THRESHOLD = 1000*1000*1000
 
 def truncate_url(path, to_length=70):
 	dirname, filename = os.path.split(path)
@@ -10,17 +10,33 @@ def truncate_url(path, to_length=70):
 	path_parts = dirname.split('/')
 	dirname += '/'
 	
-	while to_length <= len(dirname)+filename_length:
+	while to_length < len(dirname)+filename_length:
 		path_parts = path_parts[1:]
 		dirname = '.../' + '/'.join(path_parts) + '/'
-	truncated_path = dirname + filename
-	return truncated_path
+		if not path_parts:
+			break
+
+	# if filename is still to long...
+	path_length = len(dirname)+filename_length
+	if to_length < path_length:
+		filename_elipses = "[...]"
+		filestem, fileext = os.path.splitext(filename)
+		reduce_by = path_length - (to_length - len(filename_elipses))
+		print('.'*path_length + '\t path length')
+		print('.'*(to_length-(path_length-to_length)) + '\t needed change')
+		print('.'*(to_length-reduce_by) + '\t compensate for elipses')
+		filename = filestem[:-reduce_by].strip() + filename_elipses + fileext
+		print(filename)
+		print('.'*to_length)
+		assert len(filename) <= to_length, "not short enough"
+
+	return dirname + filename
+
 
 with open(sys.argv[1]) as f:
 	redundant_files = []
 	for line in f:
 		line = line.strip()
-		print("---\t{}".format(line))
 		if line:
 			if line[0] != '/':
 				new_group = {
@@ -42,13 +58,21 @@ with open(sys.argv[1]) as f:
 	# is reclaimed by deleting all files above that line
 	running_total_savings = 0
 	print('#'*80)
-	print("# {path:^69}  {savings:^5} #".format(path="Files to delete (truncated paths)", savings="Size"))
+	print("# {path:^60}  {size:^6}  {savings:^6} #".format(
+		path="Files to delete (truncated paths)",
+		size="Size",
+		savings="Save"
+	))
 	for duplicate_group in filter(large_enough_savings, redundant_files):
 		files_to_delete = duplicate_group['files'][1:]
 		for file in files_to_delete:
 			running_total_savings += duplicate_group['file_size']
-			print("# {path:>69}  {running_total_savings:>5} #".format(
-				path=truncate_url(file, to_length=69),
+			print("# {path:>60}  {size:>6}  {running_total_savings:>6} #".format(
+				path=truncate_url(file, to_length=60),
+				size=humanize.naturalsize(
+					duplicate_group['file_size'],
+					gnu=True
+				),
 				running_total_savings=humanize.naturalsize(
 					running_total_savings,
 					gnu=True
