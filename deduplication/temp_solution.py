69e34adf940e15b24d18748f1a58ef729b0d00d8
Doug McGeehan <djmvfb@mst.edu>
2016-11-11 01:21:36 -0600

    seems to be working kinda

--- deduplication/temp_solution.py
+++ deduplication/temp_solution.py
@@ -2,22 +2,23 @@ import sys
 from pprint import pprint
 
 with open(sys.argv[1]) as f:
-	new_group = {'files':[]}
-	redundant_files = [new_group]
+	redundant_files = []
 	for line in f:
 		line = line.strip()
 		print("---\t{}".format(line))
 		if line:
-			if line[0] == '/':
-				new_group['files'].append(line)
+			if line[0] != '/':
+				new_group = {
+					'files':[],
+					'savings_if_deduped': 0,
+					'file_size': int(line.split()[0])
+				}
+				redundant_files.append(new_group)
 			else:
-				new_group['file_size'] = int(line.split()[0])
+				files = new_group['files']
+				if files:
+					new_group['savings_if_deduped'] += new_group['file_size']
+				files.append(line)
 
-		else:
-			new_group = {'files':[]}
-			redundant_files.append(new_group)
-			continue
-
-
-	redundant_files.sort(key=lambda group: group['file_size'], reverse=True)
+	redundant_files.sort(key=lambda group: group['savings_if_deduped'], reverse=True)
 	pprint(redundant_files)
