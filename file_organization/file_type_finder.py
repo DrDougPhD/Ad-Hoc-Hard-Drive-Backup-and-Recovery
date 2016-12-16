3860665873a2d84842d3b91bca365b16fca0f413
Doug McGeehan <djmvfb@mst.edu>
2016-12-16 14:08:37 -0600

    format printing so that the extensions are right-justified

--- file_organization/file_type_finder.py
+++ file_organization/file_type_finder.py
@@ -12,6 +12,10 @@ def count_file_types_within(directory_to_walk):
 	for dirpath, _, filenames in os.walk(directory_to_walk):
 		for file in filenames:
 			file_prefix, file_extension = os.path.splitext(file)
+
+			if file_extension == "":
+				file_extension = "(none)"
+
 			file_type_counts[file_extension.lower()] += 1
 
 	return sorted(
@@ -22,8 +26,16 @@ def count_file_types_within(directory_to_walk):
 
 
 def write_counts_to_stdout(file_type_counts):
+	# iterate over file extensions and measure the widest one, for pretty
+	# printing
+	max_width_extension = max(
+		file_type_counts,
+		key=lambda c: len(c[0])
+	)[0]
+	max_width = len(max_width_extension)
+
 	for ext_count in file_type_counts:
-		print("{0}:\t{1}".format(ext_count[0], ext_count[1]))
+		print("{0}:\t{1}".format(ext_count[0].rjust(max_width), ext_count[1]))
 
 
 if __name__ == "__main__":
