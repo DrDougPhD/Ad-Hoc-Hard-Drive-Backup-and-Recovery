962b39e53439b7e9d64f9537bd2d9069333527c9
Doug McGeehan <djmvfb@mst.edu>
2017-04-25 13:08:20 -0500

    initial skeleton script for finding files that exist in some directories but not in a specific one

---
+++ directory_tree_walking/files_in_there_but_not_here.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+SYNOPSIS
+
+	python files_in_there_but_not_here.py [-h,--help] [-v,--verbose]
+
+
+DESCRIPTION
+
+	Given a target directory and a list of other directories, list the files
+    that exist within the other directories but are absent within the target.
+
+
+ARGUMENTS
+
+	-h, --help	        show this help message and exit
+	-v, --verbose       verbose output
+
+
+AUTHOR
+
+	Doug McGeehan <djmvfb@mst.edu>
+
+
+LICENSE
+
+	Copyright 2017  - GNU GPLv3
+
+"""
+
+__appname__ = 'files_in_there_but_not_here'
+__version__ = '0.0pre0'
+__license__ = 'GNU GPLv3'
+
+
+import argparse
+from datetime import datetime
+import sys
+import os
+import collections
+import logging
+logger = logging.getLogger(__name__)
+
+
+def main(args):
+    # Verify that the target directory is not listed twice
+    if args.target in args.others:
+        logger.warning("It's not useful to have the target directory also"
+                       " be in the other directories.")
+        logger.warning('Execution is aborted.')
+        sys.exit(1)
+
+    # Build a list of files in each of the supplied directories
+    files_in_target = list_files(within=args.target)
+
+    files_in_others = []
+    for other_dir in args.others:
+        files_in_others.append(list_files(within=other_dir))
+
+
+def list_files(within):
+    found_files = collections.defaultdict(set)
+    for subdirectory, directory_names, filenames in os.walk(within):
+        for f in filenames:
+            filepath = os.path.join(subdirectory, f)
+            file_size = os.path.getsize(filepath)
+            found_files[file_size] = filepath
+
+    return found_files
+
+
+def setup_logger(args):
+    logger.setLevel(logging.DEBUG)
+    # create file handler which logs even debug messages
+    # todo: place them in a log directory, or add the time to the log's
+    # filename, or append to pre-existing log
+    log_file = os.path.join('/tmp', '.log')
+    fh = logging.FileHandler(log_file)
+    fh.setLevel(logging.DEBUG)
+    # create console handler with a higher log level
+    ch = logging.StreamHandler()
+
+    if args.verbose:
+        ch.setLevel(logging.DEBUG)
+    else:
+        ch.setLevel(logging.INFO)
+
+    # create formatter and add it to the handlers
+    line_numbers_and_function_name = logging.Formatter(
+        "%(levelname)s [%(filename)s:%(lineno)s - %(funcName)20s() ] "
+        "%(message)s")
+    fh.setFormatter(line_numbers_and_function_name)
+    ch.setFormatter(line_numbers_and_function_name)
+    # add the handlers to the logger
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser(
+        description="Description printed to command-line if -h is called."
+    )
+    # during development, I set default to False so I don't have to keep
+    # calling this with -v
+    parser.add_argument('-o', '--output-file', dest='output_filepath',
+                        type=os.path.abspath, default=None,
+                        help='The output file to write the paths of the'
+                             ' absent files (default: write to stdout).')
+    parser.add_argument('-a', '--as-absolute-paths',
+                        dest='format_paths_to_absolute', action='store_true',
+                        default=False,
+                        help='Output file paths should be absolute paths'
+                             ' (default: False - relative paths)')
+    parser.add_argument('-v', '--verbose', action='store_true', default=False,
+                        help='Enable debugging messages (default: False)')
+    parser.add_argument('-s', '--create-copy-script', dest='script_type',
+                        choices=['cp', 'rsync', 'scp', 'mv'],
+                        default=None,
+                        help='Create a script that can be executed to'
+                             ' copy/move the missing files from the source'
+                             ' into the target (default: no script)')
+    parser.add_argument('target', metavar='TARGET_DIR', type=os.path.abspath,
+                        help='The target directory that might not contain'
+                             ' files in the other directories.')
+    parser.add_argument('others', metavar='OTHER_DIR', type=os.path.abspath,
+                        nargs='+', help='The other directories that might'
+                                        ' contain files not in the target.')
+
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    try:
+        start_time = datetime.now()
+
+        args = get_arguments()
+        setup_logger(args)
+
+        # figure out which argument key is the longest so that all the
+        # parameters can be printed out nicely
+        logger.debug('Command-line arguments:')
+        length_of_longest_key = len(max(vars(args).keys(),
+                                        key=lambda k: len(k)))
+        for arg in vars(args):
+            value = getattr(args, arg)
+            logger.debug('\t{argument_key}:\t{value}'.format(
+                argument_key=arg.rjust(length_of_longest_key, ' '),
+                value=value))
+
+        logger.debug(start_time)
+
+        main(args)
+
+        finish_time = datetime.now()
+        logger.debug(finish_time)
+        logger.debug('Execution time: {time}'.format(
+            time=(finish_time - start_time)
+        ))
+        logger.debug("#" * 20 + " END EXECUTION " + "#" * 20)
+
+        sys.exit(0)
+
+    except KeyboardInterrupt as e:  # Ctrl-C
+        raise e
+
+    except SystemExit as e:  # sys.exit()
+        raise e
+
+    except Exception as e:
+        logger.exception("Something happened and I don't know what to do D:")
+        sys.exit(1)
