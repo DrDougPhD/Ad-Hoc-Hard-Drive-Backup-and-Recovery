31c470b32fda37ce614cd8153606863c580d6b8f
Doug McGeehan <djmvfb@mst.edu>
2017-04-26 10:27:39 -0500

    skeleton unit testing

---
+++ directory_tree_walking/test_files_in_there_but_not_here.py
@@ -0,0 +1,28 @@
+import unittest
+
+class SplitPathFileTest(unittest.TestCase):
+
+    def test_sample(self):
+        self.assertTrue(True, 'Dummy test')
+
+
+class FilesystemWalkerTest(unittest.TestCase):
+
+    def test_sample(self):
+        self.assertTrue(True, 'Dummy test')
+
+
+class SameSizedFilesTest(unittest.TestCase):
+
+    def test_sample(self):
+        self.assertTrue(True, 'Dummy test')
+
+
+class FileSynchronizerScriptBuilderTest(unittest.TestCase):
+
+    def test_sample(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
